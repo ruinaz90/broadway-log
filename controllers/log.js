fd7286fde355970187a017bf65f8f558fe66c8e8
ruinaz90 <rzhao9002@gmail.com>
2022-08-19 14:52:02 +0000

    changed sort display

--- controllers/log.js
+++ controllers/log.js
@@ -2,7 +2,7 @@ const BroadwayShows = require("../models/BroadwayShows")
 module.exports = {
     getLog: async(req, res) => {
         try {
-            const showNames = await BroadwayShows.find().sort({dateSeen: 1, showName: 1})
+            const showNames = await BroadwayShows.find().sort({dateSeen: -1, showName: 1})
             const totalSeen = await BroadwayShows.countDocuments()
             res.render("log.ejs", {shows: showNames, totalSeen: totalSeen})
         }
