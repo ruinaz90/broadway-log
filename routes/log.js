995d9aae714d87324e52c0a3e1668289c4fcc658
ruinaz90 <52875611+ruinaz90@users.noreply.github.com>
2022-08-23 12:07:04 -0400

    added comments

--- routes/log.js
+++ routes/log.js
@@ -2,13 +2,17 @@ const express = require('express')
 const router = express.Router()
 const logController = require('../controllers/log')
 
+//main page
 router.get('/', logController.getLog)
 
+//add a show
 router.post('/add-show', logController.addShow)
 
+//edit and update a show
 router.get('/:id', logController.getEdit)
 router.post('/:id', logController.updateShow)
 
+//delete a show
 router.delete('/delete-show', logController.deleteShow)
 
 module.exports = router
